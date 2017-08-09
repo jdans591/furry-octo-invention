e62f8f751d05f7ae95af1c48ef4eedc0fbbe282d
jdans591 <>
2017-08-09 16:54:09 +1200

    Created a FileOperator class, which will be used to organise both existing and new methods relating to files into this class.

---
+++ src/corpus/FileOperator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corpus;
+
+import java.io.FileReader;
+
+/**
+ *Deals with operations relating to file input/output.
+ * @author Dhanasit
+ */
+public class FileOperator {
+    
+    
+    
+    public FileOperator() {
+        
+    }
+    /**Combine files into a single file name. The file is
+     * @param tag the list of tags the file must contain
+     * @param fileName  the output file name*/
+    public void combineFiles(String[] tags, String fileName) {
+        
+        FileReader fReader = new FileReader();
+    }
+    
+}
